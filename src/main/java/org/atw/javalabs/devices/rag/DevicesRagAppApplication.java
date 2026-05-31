140aff3919d6ef4f0aea123e98f874894576d8a0
GrosGlockner <ajaytechworkshop@gmail.com>
2026-05-31 18:31:43 +0200

    first commit

---
+++ src/main/java/org/atw/javalabs/devices/rag/DevicesRagAppApplication.java
@@ -0,0 +1,13 @@
+package org.atw.javalabs.devices.rag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevicesRagAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevicesRagAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/atw/javalabs/devices/rag/DevicesRagAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.atw.javalabs.devices.rag;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevicesRagAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
